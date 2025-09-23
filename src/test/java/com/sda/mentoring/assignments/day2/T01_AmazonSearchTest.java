a87b440157bb57ed38dc412f955e3b3a9d905106
ZahraAleid <zahra.eid112@gmail.com>
2025-09-23 20:51:17 +0300

    Task 1 Amazon search test

---
+++ src/test/java/com/sda/mentoring/assignments/day2/T01_AmazonSearchTest.java
@@ -0,0 +1,29 @@
+package com.sda.mentoring.assignments.day2;
+
+import com.sda.mentoring.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class T01_AmazonSearchTest extends TestBase {
+    /*
+    1. Navigate to: https://www.amazon.com
+    2. Search for different keywords: Java, Selenium
+    3. Assert that result text contains the searched word
+    4. Run tests from XML file using parameters
+
+     */
+
+    @Test
+    @Parameters("searchKeyword")
+    public void amazonSearchTest(String keyword) {
+        driver.get("https://www.amazon.com");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys(keyword);
+        driver.findElement(By.id("nav-search-submit-button")).click();
+        String pageSource = driver.getPageSource();
+        Assert.assertTrue(pageSource.contains(keyword),"Search results should contain: " + keyword);
+    }
+
+
+}
